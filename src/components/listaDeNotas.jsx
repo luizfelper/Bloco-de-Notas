b7fed52f046ac2bfc0d5cc9980663066486b0617
Luiz Felipe <luizfelper@gmail.com>
2021-08-31 10:32:08 -0300

    Criação do component listaDeNotas

---
+++ src/components/listaDeNotas.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class ListaDeNotas extends Component {
+
+    render() {
+        return (
+        <ul>
+            <li>
+                <section>
+                    <header>
+                        <h3>Título</h3>
+                    </header>
+                    <p>Escreva sua nota</p>
+                </section>
+            </li>
+            <li>
+                <section>
+                    <header>
+                        <h3>Título</h3>
+                    </header>
+                    <p>Escreva sua nota</p>
+                </section>
+            </li>
+            <li>
+                <section>
+                    <header>
+                        <h3>Título</h3>
+                    </header>
+                    <p>Escreva sua nota</p>
+                </section>
+            </li>
+        </ul>
+        );
+    }
+
+}
+
+export default ListaDeNotas;
